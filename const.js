6ffef2f2121c13b129ccb4e83f9a1626103b874d
Geovane Baldan <geovane.baldan@gmail.com>
2020-03-09 14:17:52 -0300

    [ADOC-0001] Added initial routing and internationalization setup

--- const.js
+++ const.js
@@ -1,3 +1,4 @@
 export const SERVER_PORT = 8080;
 export const BASE_API_ENDPOINT = '/api/v1';
 export const WIDGETS_ENDPOINT = '/widget';
+export const DOCUMENTS_ENDPOINT = '/document';
--- router/index.js
+++ router/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import {WIDGETS_ENDPOINT} from '../const';
+import {WIDGETS_ENDPOINT, DOCUMENTS_ENDPOINT} from '../const';
 import widgetRouter from './widget/index';
+import documentsRouter from './document/index';
 
 const router = express.Router();
 
 router.use(WIDGETS_ENDPOINT, widgetRouter);
+
+router.use(DOCUMENTS_ENDPOINT, documentsRouter);
 
 router.get('/', (req, res) =>
 	res.send(req.__('Hello world!'))
