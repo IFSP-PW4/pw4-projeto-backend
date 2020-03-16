a625db4ca2056f8a1ec1554dc48e812cc2ab2a9e
Pedro Augusto de Almeida Caballero <caballero@onovolab.com>
2020-03-16 11:00:40 -0300

    [FLOW-0001] create basic routes

--- const.js
+++ const.js
@@ -2,3 +2,4 @@ export const SERVER_PORT = 8080;
 export const BASE_API_ENDPOINT = '/api/v1';
 export const WIDGETS_ENDPOINT = '/widget';
 export const DOCUMENTS_ENDPOINT = '/document';
+export const FLOW_MANAGEMENT_ENDPOINT = '/flow-management'
--- router/index.js
+++ router/index.js
@@ -1,13 +1,16 @@
 import express from 'express';
-import {WIDGETS_ENDPOINT, DOCUMENTS_ENDPOINT} from '../const';
+import {WIDGETS_ENDPOINT, DOCUMENTS_ENDPOINT, FLOW_MANAGEMENT_ENDPOINT} from '../const';
 import widgetRouter from './widget/index';
 import documentsRouter from './document/index';
+import flowManagementRouter from './flow-management';
 
 const router = express.Router();
 
 router.use(WIDGETS_ENDPOINT, widgetRouter);
 
 router.use(DOCUMENTS_ENDPOINT, documentsRouter);
+
+router.use(FLOW_MANAGEMENT_ENDPOINT, flowManagementRouter);
 
 router.get('/', (req, res) =>
 	res.send(req.__('Hello world!'))
