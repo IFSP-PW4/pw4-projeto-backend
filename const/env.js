f513e5089c224112507450f84201cb4ee1de0120
Gabriel Vander <gabsvander@hotmail.com>
2020-07-03 15:01:13 -0300

    [#1] Renamed SERVER_PORT environment variable in env.js

--- const/env.js
+++ const/env.js
@@ -1,4 +1,4 @@
-export const SERVER_PORT = 8080;
+export const PORT = process.env.PORT || 8080;
 export const BASE_API_ENDPOINT = '/api/v1';
 export const WIDGETS_ENDPOINT = '/widgets';
 export const DOCUMENTS_ENDPOINT = '/documents';
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import {BASE_API_ENDPOINT, SERVER_PORT} from './const/env';
+import {BASE_API_ENDPOINT, PORT} from './const/env';
 import express from 'express';
 import helmet from 'helmet';
 import i18n from 'i18n';
@@ -17,6 +17,6 @@ app.use(i18n.init);
 
 app.use(BASE_API_ENDPOINT, router);
 
-app.listen(SERVER_PORT, () =>
-	console.log(`Server running on port ${SERVER_PORT}`)
+app.listen(PORT, () =>
+	console.log(`Server running on port ${PORT}`)
 );
