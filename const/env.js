25951ee104019de07b890e00e4bc8ab4e7871d94
Geovane Baldan <geovane.baldan@gmail.com>
2020-07-13 23:28:06 -0300

    [#13] Added MongoDB Atlas connection string

--- const/env.js
+++ const/env.js
@@ -1,5 +1,5 @@
 export const PORT = process.env.PORT || 8080;
-export const DB_HOST = process.env.DB_HOST || '';
+export const DB_HOST = process.env.DB_HOST || 'maincluster.5qpft.mongodb.net';
 export const DB_USER = process.env.DB_USER || '';
 export const DB_PASSWORD = process.env.DB_PASSWORD || '';
 export const DB_DATABASE_NAME = process.env.DB_DATABASE_NAME || '';
