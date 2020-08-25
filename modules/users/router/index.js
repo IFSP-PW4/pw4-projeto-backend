012013e79f0654cc831f9aa4b0637adca887b6cd
gabriel96kt <gabriel96kt@gmail.com>
2020-08-25 15:09:02 -0300

    Update index.js

--- modules/users/router/index.js
+++ modules/users/router/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 // importando módulo body-parser para pegar dados enviados dentro do corpo da requisição.
 import bodyParser from 'body-parser';
 import createNewUser from '../controller/userController';
+import {registerUser, logUser, getAllUsers, getUser} from '../controller/userController';
 
 
 const usersRouter = Router();
@@ -18,10 +19,13 @@ usersRouter.post('/registerUser', (req, res) => {
 	const password = req.body.password;
 	const email = req.body.email;
 	res.send(createNewUser(password,email));
+	res.send(registerUser(password,email));
 });
 
 usersRouter.get('/:userId', (req, res) => {
 	res.send(res.__('Returning user with id {{userId}}', { userId: req.params.userId }));
+usersRouter.get('/:userEmail', (req, res) => {
+	res.send(getUser(email));
 });
 
 usersRouter.put('/:userId', (req, res) => {
